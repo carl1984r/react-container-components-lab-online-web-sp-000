adc874f6f6d24dd1654b913765a15d6c753502df
Bry Cox <carl.cc@gmail.com>
2020-04-06 08:15:35 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,47 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+            class SearchableMovieReviewsContainer extends Component {
+            
+                state = {
+                    reviews: [],
+                    searchTerm: 'dog'
+                }
+            
+                fetchURL = () => {
+                    fetch(BASE_URL.concat(this.state.searchTerm))
+                    .then(res => res.json())
+                    .then(json => {
+                        this.setState({
+                            reviews: json.results
+                        })
+                    })
+                    .catch(console.log("bad"))
+                }
+            
+                handleChange = (event) => {
+                    event.preventDefault()
+                    this.setState({
+                        searchTerm: event.target.value
+                    })
+                }
+            
+                componentDidMount() {
+                    this.fetchURL()
+                }
+            
+            
+                render() {
+                    return (
+                        <div>
+                             <form>
+                                <input onChange={this.handleChange}></input>
+                            </form>
+                            <button onClick={this.fetchURL}>Search</button>
+                            {<MovieReviews reviews={this.state.reviews}/>}
+                        </div>
+                    )
+                }
+            }
+            
+            export default SearchableMovieReviewsContainer
