3e4225a63d688ac1f0cf23dd70f524d725bd43fe
Bry Cox <carl.cc@gmail.com>
2020-04-06 08:19:02 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,47 +6,47 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const BASE_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-            class SearchableMovieReviewsContainer extends Component {
+class SearchableMovieReviewsContainer extends Component {
 
-                state = {
-                    reviews: [],
-                    searchTerm: 'dog'
-                }
+    state = {
+        reviews: [],
+        searchTerm: 'dog'
+    }
 
-                fetchURL = () => {
-                    fetch(BASE_URL.concat(this.state.searchTerm))
-                    .then(res => res.json())
-                    .then(json => {
-                        this.setState({
-                            reviews: json.results
-                        })
-                    })
-                    .catch(console.log("bad"))
-                }
+    fetchURL = () => {
+        fetch(BASE_URL.concat(this.state.searchTerm))
+        .then(res => res.json())
+        .then(json => {
+            this.setState({
+                reviews: json.results
+            })
+        })
+        .catch(console.log("bad"))
+    }
 
-                handleChange = (event) => {
-                    event.preventDefault()
-                    this.setState({
-                        searchTerm: event.target.value
-                    })
-                }
+    handleChange = (event) => {
+        event.preventDefault()
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
 
-                componentDidMount() {
-                    this.fetchURL()
-                }
+    componentDidMount() {
+        this.fetchURL()
+    }
 
 
-                render() {
-                    return (
-                        <div>
-                             <form>
-                                <input onChange={this.handleChange}></input>
-                            </form>
-                            <button onClick={this.fetchURL}>Search</button>
-                            {<MovieReviews reviews={this.state.reviews}/>}
-                        </div>
-                    )
-                }
-            }
+    render() {
+        return (
+            <div>
+                 <form>
+                    <input onChange={this.handleChange}></input>
+                </form>
+                <button onClick={this.fetchURL}>Search</button>
+                {<MovieReviews reviews={this.state.reviews}/>}
+            </div>
+        )
+    }
+}
 
-            export default SearchableMovieReviewsContainer
+export default SearchableMovieReviewsContainer
